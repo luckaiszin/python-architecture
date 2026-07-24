e8a7e380b095991e41a101fe05de2f4a52375219
luckaiszin <pateslucas@gmail.com>
2026-07-24 10:35:51 -0300

    adicionando basis python

---
+++ base/walrus_operator/app.py
@@ -0,0 +1,8 @@
+# É útil quando se cria e logo já se usa a variável
+
+import time
+
+#comando = input('Digite o comando: ')
+
+while(comando := input('Digite o comando: ')) != 'exit':
+    print(comando)
